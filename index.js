a0cbfe94aeb40ac9bfc2eead258f7e8749ee4a2b
MateuszMazurTUL <MateuszMazurTUL@edu.p.lodz.pl>
2022-02-12 22:42:06 +0100

    temp fix

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ class dniPracy {
     }
 }
 
-a = new dniPracy(new startPracy(new Date(2021, 10, 10), new Time(1, 0, 0, 0)), 8);
+//a = new dniPracy(new startPracy(new Date(2021, 10, 10), new Time(1, 0, 0, 0)), 8);
 
 MongoClient.connect(url, {}, (error, client)=>{
     if (error) { console.log("not ok")}

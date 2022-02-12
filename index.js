2ec286a293e7820d7ae10f1556354d3b61091d09
MateuszMazurTUL <MateuszMazurTUL@edu.p.lodz.pl>
2022-02-12 23:14:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -73,14 +73,14 @@ a = new dniPracy(new startPracy(new Date(2021, 10, 10), new Time(1, 0, 0, 0)), 8
 
 MongoClient.connect(url, {}, (error, client)=>{
     if (error) { console.log("not ok")}
-
-    const db = client.db(dbname)
-
-    query = {id:1}
-
-    res = db.collection("test1").find({}).toArray(function(err, result) {
-        if (err) throw err;
-        console.log(result);
-      });
+console.log("almoost ok")
+//    const db = client.db(dbname)
+//
+//    query = {id:1}
+//
+//    res = db.collection("test1").find({}).toArray(function(err, result) {
+//        if (err) throw err;
+//        console.log(result);
+//      });
 
 })

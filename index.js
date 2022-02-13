19beb7335245343a6cc24dc0dee9cf759119c61e
MateuszMazurTUL <MateuszMazurTUL@edu.p.lodz.pl>
2022-02-13 21:07:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -160,6 +160,4 @@ try {
 } catch (error) {
   console.error("error db");
   console.error(error);
-   expected output: ReferenceError: nonExistentFunction is not defined
-   Note - error messages will vary depending on browser
 }

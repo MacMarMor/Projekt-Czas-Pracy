c9adc813feab8a35543bcbcec7070b25df3c45c8
MateuszMazurTUL <MateuszMazurTUL@edu.p.lodz.pl>
2022-02-13 20:29:05 +0100

    db

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const MongoClient = require('mongodb').MongoClient
 //const url = "mongodb://SieciWWW:Sieci123@sieciwww.ydgvt.mongodb.net/test"
 //const url = "mongodb://SieciWWW:Sieci123@sieciwww.ydgvt.mongodb.net:5000/Test1"
 //const url = "mongodb://SieciWWW:Sieci123@sieciwww.ydgvt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-const url = "mongodb://SieciWWW:Sieci123:@sieciwww.ydgvt.mongodb.net:27017/?ssl=true&replicaSet=&authSource=admin&retryWrites=true&w=majority"
+//const url = "mongodb://SieciWWW:Sieci123@sieciwww.ydgvt.mongodb.net/?ssl=true&replicaSet=&authSource=admin&retryWrites=true&w=majority"
 const dbname = "Test1";
 
 class Date
@@ -97,6 +97,20 @@ class dniPracy {
 
 console.log("test log");
 a = new dniPracy(new startPracy(new Date(2021, 10, 10), new Time(1, 0, 0, 0)), 8);
+
+var MongoClient = require('mongodb').MongoClient;
+var uri = "mongodb://SieciWWW:Sieci123@sieciwww-shard-00-00.ydgvt.mongodb.net:27017,sieciwww-shard-00-01.ydgvt.mongodb.net:27017,sieciwww-shard-00-02.ydgvt.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-t0v948-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+MongoClient.connect(uri, function(err, client) {
+  //const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+    
+        console.error("buka db");
+  client.close();
+});
+
+
+
 
 try {
     MongoClient.connect(url, {}, (error, client)=>{

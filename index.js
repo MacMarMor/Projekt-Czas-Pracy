6896d3a2b771a8e9863a91b63e9ba36aeec5e6bd
MateuszMazurTUL <MateuszMazurTUL@edu.p.lodz.pl>
2022-02-13 14:37:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const path = require('path')
-const bodyParser = require('body-parser'),
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));

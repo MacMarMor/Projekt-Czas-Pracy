974cdc5df0f2018ade4e3d9da16c6fc6d764d984
MateuszMazurTUL <MateuszMazurTUL@edu.p.lodz.pl>
2022-02-17 00:37:14 +0100

    Update index.js

--- public/generuj_grafik/index.js
+++ public/generuj_grafik/index.js
@@ -1,6 +1,10 @@
+alert("result");
+console.log()("result");
 $(".button_sterowanie_aplikacji2").bind('ajax:success', function(result) {
     alert(result);
+	console.log()(result);
 });
 $("#ramka_generuj").bind('ajax:success', function(result) {
     alert(result);
+	console.log()(result);
 });

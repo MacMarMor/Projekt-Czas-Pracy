8a89538b45b863b4d8b9fa7a5dc9c2da16a738b5
MateuszMazurTUL <MateuszMazurTUL@edu.p.lodz.pl>
2022-02-17 00:46:53 +0100

    gbv

--- public/generuj_grafik/index.js
+++ public/generuj_grafik/index.js
@@ -1,10 +1,10 @@
-alert("result");
-console.log("result");
-$(".button_sterowanie_aplikacji2").bind('ajax:success', function(result) {
+alert("start");
+console.log("start");
+/*$(".button_sterowanie_aplikacji2").bind('ajax:success', function(result) {
     alert(result);
 	console.log()(result);
-});
-$("#ramka_generuj").bind('ajax:success', function(result) {
+});*/
+$("#form_generuj").bind('ajax:success', function(result) {
     alert(result);
 	console.log()(result);
 });

9223ede6f3a14741f1492c65579b1d224d99c746
Karo0510 <karma0510@gmail.com>
2022-02-19 17:02:08 +0100

    JSON to Object3

--- index.js
+++ index.js
@@ -50,8 +50,15 @@ app.post('/generate', (req, res) => {
 	var promise = db.getEmployees();
 	promise.then(function(json) {
         EmpS.updateEmployees(json);
-        EmpS.setSchedule();
+        
 	});
+
+    var promise2 = db.getSchedule();
+	promise2.then(function(json) {
+        EmpS.updateMinimumStaff(json);
+	});
+
+    EmpS.setSchedule();
 
 	/*var promise2 = db.getSchedule();
 	promise2.then(function(json) {

090fe8b314e01e8ae5f7e8fed61f1008a58cbc17
Karo0510 <karma0510@gmail.com>
2022-02-19 14:30:03 +0100

    JSON to Object3

--- EmployeesScheduler.js
+++ EmployeesScheduler.js
@@ -87,7 +87,7 @@ class EmployeesScheduler {
 
         //console.log(typeof(json))
         var obj = JSON.parse(JSON.stringify(json));
-        console.log("obj = "+obj[0].dniPracy);
+        console.log("obj = "+obj[0].dniPracy[0]);
 		
 		this.EMPLOYEE = json;
     }

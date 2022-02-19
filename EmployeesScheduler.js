4830010fc34f51264b8deb927f77270c8dbdae25
Karo0510 <karma0510@gmail.com>
2022-02-19 21:00:00 +0100

    JSON to Object3

--- EmployeesScheduler.js
+++ EmployeesScheduler.js
@@ -111,7 +111,7 @@ class EmployeesScheduler {
 
         var obj = JSON.parse(JSON.stringify(json));
 
-        console.log("upadte mininum staff = "+obj[0].grafik["2022-02-19"]);
+        console.log("upadte mininum staff = "+obj[0].grafik["2022-02-19"].dane);
 
         //this.MINIMUMSTAFF = obj[0].grafik[0];
         //console.log(this.MINIMUMSTAFF.length);

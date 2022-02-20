4f2a9b6792e43f6c06a112cbe35b4548b4a9cf7c
Karo0510 <karma0510@gmail.com>
2022-02-20 14:04:09 +0100

    JSON to Object3

--- EmployeesScheduler.js
+++ EmployeesScheduler.js
@@ -166,7 +166,7 @@ class EmployeesScheduler {
     {
         for (var j = hour; j < hour+employee.staz; j++)
         {
-            this.MINIMUMSTAFF[j].iloscOsob = (this.MINIMUMSTAFF[j].iloscOsob)-1;
+            //this.MINIMUMSTAFF[j].iloscOsob = (this.MINIMUMSTAFF[j].iloscOsob)-1;
             console.log(this.MINIMUMSTAFF[j].iloscOsob);
             //this.MINIMUMSTAFF[j].pracownicy.push(employee);
         }
